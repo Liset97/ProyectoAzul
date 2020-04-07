b1e542fa82302eb697a9f539ee9dac5144d9a4aa
LSilvaO <lisetsilvao2003@gmail.com>
2020-04-07 14:01:50 -0400

    Segundo Commit
    Aqui se redefinieron algunos elementos de la based de datos y se
    realizo el llenado de las factorias, seleccionando un color y sacandolo
    de la bolsa.

--- bd.pl
+++ bd.pl
@@ -4,7 +4,7 @@ linea(CantL,Color).
 
 % Definicion de Patron, donde:
 % X = 1, Y = 2, Z = 3, V = 4, W = 5.... donde cada uno de ellos representan el numero de fichas en la linea correspondiente.
-patron(linea(X,Color1),linea(Y,Color2),linea(Z,Color3),linea(V,Color4),linea(W,Color5)):- X =< 1, Y =< 2, Z =< 3, V =< 4, W =< 5.
+patron(linea(X,C1),linea(Y,C2),linea(Z,C3),linea(V,C4),linea(W,C5)):- X =< 1, Y =< 2, Z =< 3, V =< 4, W =< 5.
 
 % f(Q,W,E,R,T) representa las filas de la pared d0nde tiene una ficha x cada color como maximo.
 f(Negro,Azul,Amarillo,Rojo,Blanco):- Negro =< 1, Azul =< 1, Amarillo =< 1, Rojo =< 1, Blanco =< 1.
@@ -19,6 +19,7 @@ pared(f(X1,X2,X3,X4,X5),f(Y1,Y2,Y3,Y4,Y5),f(Z1,Z2,Z3,Z4,Z5),f(V1,V2,V3,V4,V5),f(
 % Tercer parametro, Patron, es una estructura que guarda la cantidad de fichas que hay en cada linea con el color correspondiente.
 % Cuarto parametro la Pared,
 % Quinto parametro es un numeroque representa la cantidad de fichas en el suelo.
+:- dynamic jugador/5.
 jugador(a, 0, patron(linea(0,none),linea(0,none),linea(0,none),linea(0,none),linea(0,none)), pared(f(0,0,0,0,0),f(0,0,0,0,0),f(0,0,0,0,0),f(0,0,0,0,0),f(0,0,0,0,0)), 0).
 jugador(b, 0, patron(linea(0,none),linea(0,none),linea(0,none),linea(0,none),linea(0,none)), pared(f(0,0,0,0,0),f(0,0,0,0,0),f(0,0,0,0,0),f(0,0,0,0,0),f(0,0,0,0,0)), 0).
 jugador(c, 0, patron(linea(0,none),linea(0,none),linea(0,none),linea(0,none),linea(0,none)), pared(f(0,0,0,0,0),f(0,0,0,0,0),f(0,0,0,0,0),f(0,0,0,0,0),f(0,0,0,0,0)), 0).
@@ -27,6 +28,7 @@ jugador(d, 0, patron(linea(0,none),linea(0,none),linea(0,none),linea(0,none),lin
 % Espacio de definicion de las factorias.
 % factoria(No,C1,C2,C3,C4).
 % donde No representa el numero de la factoria, Ci los colores de las 4 fichas.
+:- dynamic factoria/5.
 factoria(1,none,none,none,none).
 factoria(2,none,none,none,none).
 factoria(3,none,none,none,none).
@@ -38,14 +40,28 @@ factoria(8,none,none,none,none).
 factoria(9,none,none,none,none).
 
 % Espacio de definicion del centro de Mesa.
-cM(Cnegro,Cazul,Camarillo,Crojo,Cblanco,Total):- Total is Cnegro + Cazul + Camarillo + Crojo + Cblanco.
+:- dynamic cM/2.
+cM(negro,0).
+cM(azul,0).
+cM(amarillo,0).
+cM(rojo,0).
+cM(blanco,0).
 
 % Espacio de definicion Bolsa:
-% Bolsa(Negro,Azul,Amarillo,Rojo,Blanco,Total) inicialmente se cuenta con esta cantidaden la bolsa.
-bolsa(20,20,20,20,20,100).
+% bolsa(Negro,Azul,Amarillo,Rojo,Blanco,Total) inicialmente se cuenta con esta cantidad en la bolsa.
+:- dynamic bolsa/2.
+bolsa(negro,20).
+bolsa(azul,20).
+bolsa(amarillo,20).
+bolsa(rojo,20).
+bolsa(blanco,20).
 
 % Espacio de definicion de los colores:
-colores([negro,azul,amarillo,rojo,blanco]).
+colores(0,negro).
+colores(1,azul).
+colores(2,amarillo).
+colores(3,rojo).
+colores(4,blanco).
 
 
 
--- fases.pl
+++ fases.pl
@@ -1,5 +1,16 @@
 :- [bd].
 
+% Primero se escoge al jugador inicial ademas se llenan las facttorias
+
+% Estos metodos son para escoger color, sacar de3 la bolsa y llenar las factorias.
+
+cogeColor(R):- random(0,5,U), colores(U,R).
+
+cogeBolsa(R):- cogeColor(R), bolsa(R,X1), X1 > 0, X is X1-1, retract(bolsa(R,X1)), assert(bolsa(R,X)).
+
+llenaFact(X,R1,R2,R3,R4):- cogeBolsa(R1),cogeBolsa(R2),cogeBolsa(R3),cogeBolsa(R4), retract(factoria(X,none,none,none,none)), assert(factoria(X,R1,R2,R3,R4)).
+llenaTodasFact(N):- N > 0, llenaFact(N,T1,T2,T3,T4), N1 is N-1, llenaTodasFact(N1). 
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %                                     %
 %   PRIMERA FASE: "OFERTA FACTORIA"   %
